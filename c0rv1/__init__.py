88205ee8c306d61e0951654c2b006540799f20b3
Daan Gerits <daan@pryml.io>
2020-05-13 19:50:04 +0200

    added the aht10 library

--- c0rv1/__init__.py
+++ c0rv1/__init__.py
@@ -1,11 +1,22 @@
 from buzzer import BuzzerPlayer
 import machine, neopixel
+from lis2dh12 import LIS2DH12_I2C
+from aht10 import AHT10
 
 PIN_BUZZER = 32
 PIN_NEOPIXEL = 2
+PIN_I2C_SCL = 22
+PIN_I2C_SDA = 21
+
+
+I2C_ADDR_LIS2D = 0x18
+I2C_ADDR_AHT10 = 0x38
 
 class C0rv1:
     def __init__(self):
         self.buzz = BuzzerPlayer(pin=PIN_BUZZER)
         self.pixels = neopixel.NeoPixel(machine.Pin(PIN_NEOPIXEL), 5)
-        pass
+
+        self.i2c = machine.I2C(scl=machine.Pin(PIN_I2C_SCL), sda=machine.Pin(PIN_I2C_SDA), freq=100000)
+        self.acc = LIS2DH12_I2C(i2c, address=I2C_ADDR_LIS2D)
+        self.aht10 = AHT10(i2c, address=I2C_ADDR_AHT10)
---
+++ c0rv1/aht10.py
@@ -0,0 +1,37 @@
+from machine import Pin, I2C
+import time
+
+AHT10_ADDRESS = 0x38
+AHT10_INIT_CMD = 0b11100001
+AHT10_TRIGGER_CMD = 0b10101100
+AHT10_RESET_CMD = 0b10111010
+
+AHT10_STATUS_BUSY_MASK = 1<<7
+AHT10_STATUS_MODE_MASK = 0b11<<5
+AHT10_STATUS_CALIBRATED_MASK = 1<<3
+
+class AHT10:
+    def __init__(self, i2c, address=0x38):
+        self.i2c = i2c
+        self.address = address
+    
+    def status(self):
+        status = self.i2c.readfrom(self.address, 1)[0]
+        return {
+            "status": "{:#010b}".format(status),
+            "busy": (s & AHT10_STATUS_BUSY_MASK) >> 7),
+            "mode": (s & AHT10_STATUS_MODE_MASK) >> 5),
+            "calibrated": (s & AHT10_STATUS_CALIBRATED_MASK) >> 3)
+        }
+
+    def read(self):
+        data = self.i2c.readfrom(self.address, 6)
+        rh_data = data[1]<<12 | data[2]<<4 | data[3]>>4
+        rh = rh_data * 100/2**20
+        t_data = (data[3] & 0x0F)<<16 | data[4]<<8 | data[5]
+        t = ((t_data*200)/2**20)-50
+        return t, rh
+
+    def busy(self):
+        status = self.i2c.readfrom(self.address, 1)[0]
+        return bool(status & AHT10_STATUS_BUSY_MASK)
